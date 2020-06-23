cc46f42661ccfe6cdeff9ad5684705b2e9fbbfaf
Wanda Pirjol <wanda.chereji@3pillarglobal.com>
2020-06-23 15:24:52 +0300

    added wger login credentials

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,8 @@
 const Constants = {
+    // wger test credentials for https://wger.de/en/user/login
+    WGER_USER: "test.dev",
+    WGER_PASS: "test123",
+
     WGER_API_KEY: "53a73005874c3695a00755f3dd6470413b5b39fa",
     WGER_API_PATH: "https://wger.de/api/v2/",
     WGER_WORKOUT_ENDPOINT: "workout/"
