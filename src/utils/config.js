cdbc5234210f4bc3cc39fef7c0021569869f3466
Wanda Pirjol <wanda.chereji@3pillarglobal.com>
2020-06-23 15:03:31 +0300

    added wger API KEY

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const Constants = {
-    WGER_API_KEY: "ee96f25532eae3aacac8678971b76a00b3b1fdb4", // todo not registerred
+    WGER_API_KEY: "53a73005874c3695a00755f3dd6470413b5b39fa",
     WGER_API_PATH: "https://wger.de/api/v2/",
     WGER_WORKOUT_ENDPOINT: "workout/"
 };
