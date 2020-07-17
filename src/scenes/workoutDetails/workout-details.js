b5c83657835a6fbdd2aae74582be76e9f393e8cb
Wanda Pirjol <wanda.chereji@3pillarglobal.com>
2020-07-17 14:19:44 +0300

    displayed workout goalon the workout details screen

--- src/scenes/workoutDetails/workout-details.js
+++ src/scenes/workoutDetails/workout-details.js
@@ -23,10 +23,25 @@ const SectionHeader = ({ title }) => (
     <Text style={styles.sectionHeader}>{title}</Text>
 );
 
+function Screen({data, navi, goal}) {
+    return (
+        <View>
+            <Text style={styles.goal}>Goal: {goal}</Text>
+            <SectionList
+                sections={data}
+                renderItem={({item}) => <Item text={item} navi={navi}/>}
+                renderSectionHeader={({section}) => <SectionHeader title={section.title}/>}
+                keyExtractor={(item, index) => index}
+            />
+        </View>
+    );
+}
+
 
 const WorkoutDetailsScreen = ({route, navigation}) => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [goal, setGoal] = useState("");
 
     function processWorkoutByIdServiceResult(apiCallOutcome, responseJson) {
         switch(apiCallOutcome) {
@@ -48,6 +63,7 @@ const WorkoutDetailsScreen = ({route, navigation}) => {
                     data = [...data, exercise];
                 });
 
+                setGoal(responseJson.obj.comment);
                 setData(data);
                 break;
     
@@ -69,49 +85,49 @@ const WorkoutDetailsScreen = ({route, navigation}) => {
     return (
         <View style={styles.container}>
             {isLoading ? <ActivityIndicator/> : (
-                // <Text>Goal: {data.obj.comment}</Text>
-
-                <SectionList
-                    sections={data}
-                    renderItem={({item}) => <Item text={item} navi={navigation}/>}
-                    renderSectionHeader={({section}) => <SectionHeader title={section.title}/>}
-                    keyExtractor={(item, index) => index}
-                />
-            )}
-
-            
+                <Screen data ={data} navi={navigation} goal={goal}/>
+            )} 
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-     flex: 1,
-     paddingTop: 20,
-     backgroundColor: 'white',
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: 'white',
     },
     sectionHeader: {
-      paddingTop: 4,
-      paddingLeft: 12,
-      paddingRight: 12,
-      paddingBottom: 4,
-      fontSize: 10,
-      color: 'tomato',
-      textTransform: 'uppercase'
+        paddingTop: 4,
+        paddingLeft: 12,
+        paddingRight: 12,
+        paddingBottom: 4,
+        fontSize: 10,
+        color: 'tomato',
+        textTransform: 'uppercase'
     },
     item: {
-      padding: 8,
-      marginLeft: 16,
-      marginRight: 16,
-      marginTop: 8,
-      marginBottom: 8,
-      fontSize: 18,
-      minHeight: 44,
-      backgroundColor: 'white',
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 8,
-      color: 'gray',
+        padding: 8,
+        marginLeft: 16,
+        marginRight: 16,
+        marginTop: 8,
+        marginBottom: 8,
+        fontSize: 18,
+        minHeight: 44,
+        backgroundColor: 'white',
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 8,
+        color: 'gray',
+    },
+    goal: {
+        paddingTop: 4,
+        paddingLeft: 12,
+        paddingRight: 12,
+        paddingBottom: 12,
+        fontSize: 18,
+        color: 'gray',
+        textTransform: 'uppercase'
     },
 })
 
---
+++ src/services/GetExerciseImage.js
@@ -0,0 +1,2 @@
+// curl -X GET https://wger.de/api/v2/exerciseimage/382/thumbnails/ \
+//       -H 'Authorization: Token 53a73005874c3695a00755f3dd6470413b5b39fa'
