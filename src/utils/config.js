6eeb9ac0bdd54a2b7f19d29f4c667b605c74d71f
Wanda Pirjol <wanda.chereji@3pillarglobal.com>
2020-07-27 14:19:27 +0300

    orderred workout list

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const Constants = {
 
     WGER_API_KEY: "53a73005874c3695a00755f3dd6470413b5b39fa",
     WGER_API_PATH: "https://wger.de/api/v2/",
-    WGER_WORKOUT_ENDPOINT: "workout/",
+    WGER_WORKOUT_ENDPOINT: "workout/?ordering=-id",
     WGER_WORKOUT_BY_ID_ENDPOINT: "workout/<id>/canonical_representation/",
     WGER_EXERCISE_IMAGE_ENDPOINT: "exerciseimage/<id>/thumbnails/",
 
